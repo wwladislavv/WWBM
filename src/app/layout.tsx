db47fa2a9272cb647075ba4649da1e06e8cb3a2c
Vladyslav Koponkin <vladyslav.k@collectius.com>
2024-01-27 10:56:54 +0000

    update lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: "WWBM",
-    description: "Who Wants to Be a Millionaire?",
+    title: 'WWBM',
+    description: 'Who Wants to Be a Millionaire?',
 };
 
 export default function RootLayout({
