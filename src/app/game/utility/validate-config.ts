272a39be2aad6f56dba6e9844c15192e87b0d795
frontend_vladyslav <kopenkin8@gmail.com>
2024-02-05 14:23:26 +0000

    fix tests

--- src/app/game/utility/validate-config.ts
+++ src/app/game/utility/validate-config.ts
@@ -8,29 +8,29 @@ export default function validate(config: Config) {
         return errors;
     }
 
-    config.forEach(({ question, award, answers }, index) => {
+    /* eslint-disable-next-line no-plusplus */
+    for (let i = 0; i < 12; i++) {
+        const { question, award, answers } = config[i];
         if (!question.length) {
-            errors[index] = 'Question is required';
-            return;
+            errors[i] = 'Question is required';
+            break;
         }
-
         if (award <= 0) {
-            errors[index] = 'Award must be greater than 0';
-            return;
+            errors[i] = 'Award must be greater than 0';
+            break;
         }
-
         if (answers.length < 4) {
-            errors[index] = 'At least 4 answers are required';
-            return;
+            errors[i] = 'At least 4 answers are required';
+            break;
         }
         if (answers.filter((a) => a.correct).length < 1) {
-            errors[index] = 'At least one correct answer is required';
-            return;
+            errors[i] = 'At least one correct answer is required';
+            break;
         }
         if (answers.some((a) => !a.value.length)) {
-            errors[index] = 'All answers must have a value';
+            errors[i] = 'All answers must have a value';
         }
-    }, []);
+    }
 
     return errors;
 }
